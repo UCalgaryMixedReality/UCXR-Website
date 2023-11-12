f16524cb6e238a0ba659191635a085019ea3d482
aadic0 <aadichauhan321@gmail.com>
2023-11-12 03:43:15 -0700

    findPosition function update
    Added findPosition to HandDetector class. This function will allow us to get the position of each landmark.

--- ARMEDhgr/tfod/main.py
+++ ARMEDhgr/tfod/main.py
@@ -17,13 +17,29 @@ class HandDetector():
     def findHands(self, img, draw=True):
         # Method to find hands in the input image and draw landmarks and connections if required
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
+        self.results = self.hands.process(imgRGB)
 
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
                 if draw: # set as true in function definition
                     self.mpDraw.draw_landmarks(img, handLms, self.mphands.HAND_CONNECTIONS)
         return img
+    
+    def findPosition(self, img, handNo=0, draw=True):
+        lmList = [] # List to store and return landmark positions
+
+
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape # Get height width and channel of image
+                cx, cy = int(lm.x * w), int(lm.y * h) # Calculate pixel coordinates of landmark
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED) # Will draw a filled circle at each landmark so itll be easier to see them
+
+        return lmList
+
 
 def main(): 
     # Main function for running the hand tracking application
@@ -45,6 +61,11 @@ def main():
 
         # Use the HandDetector to find and draw hands in the frame
         img = detector.findHands(img)
+
+
+        # Use the HandDetector to find the position of the hands in the frame and send the coordinates to lmList
+        lmList = detector.findPosition(img)
+        print(lmList) # we can index this variable to obtain the position of each joint/part of the hand
 
         # Calculate and display frames per second
         cTime = time.time()
