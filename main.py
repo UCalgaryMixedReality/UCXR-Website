d93a53476e7bde989b1928c0942a97c13a49a77d
aadic0 <aadichauhan321@gmail.com>
2024-03-01 18:37:24 -0700

    main.py changes

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ from ARmedHGR import HandTrack as ht
 from ARmedHGR import point as pt
 from tfod import app
 
+import csv
+import copy
+import argparse
+import itertools
+from collections import Counter
+from collections import deque
+
+import cv2 as cv
+import numpy as np
+import mediapipe as mp
+
+from tfod.utils import CvFpsCalc
+from tfod.model import KeyPointClassifier
+from tfod.model import PointHistoryClassifier
