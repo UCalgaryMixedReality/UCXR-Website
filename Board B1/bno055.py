d0ff8d293e6ee4e9b6dde2aafe1187ef33a99c9a
toluw4nimi <147214823+toluw4nimi@users.noreply.github.com>
2024-03-23 15:09:31 -0600

    Add files via upload

---
+++ Board B1/bno055.py
@@ -0,0 +1,35 @@
+# bno055.py MicroPython driver for Bosch cls nine degree of freedom inertial
+# measurement unit module with sensor fusion.
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Radomir Dopieralski for Adafruit Industries.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# This is a port of the Adafruit CircuitPython driver to MicroPython, with
+# modified/enhanced functionality.
+
+# Original Author: Radomir Dopieralski
+# Ported to MicroPython and extended by Peter Hinch
+# This port copyright (c) Peter Hinch 2019
+
+from micropython import const
+from bno055_base import BNO055_BASE
+
