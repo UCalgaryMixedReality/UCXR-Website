afc2f02c32bbea85b79d2851ccb1c453d69141e7
toluw4nimi <147214823+toluw4nimi@users.noreply.github.com>
2024-03-23 20:01:33 -0600

    Add files via upload

---
+++ Board B1/B1_Output_SerialMonitor.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//From the board manager, install, 'Arduino mBed OS RP2040 Boards' by Arduino
+//Connect the board 
+//Select the Board 'Raspberry Pi Pico'
+//Select the booard's port (COM3, COM4 etc.)
+//Open the serial monitor
+//You can also view the serial plotter to see an interesting plot of how your values change in real time
