fc52e55483c284391299e3f4be5697a932365b13
aadic0 <aadichauhan321@gmail.com>
2024-03-30 14:48:40 -0600

    Fix Index Error of Point.py

--- tfod/point.py
+++ tfod/point.py
@@ -6,27 +6,31 @@ import numpy as np
 import math
 
 def point(img, lmList0, lmList1):
+    
 
     if len(lmList0) == 0 and len(lmList1) == 0:
         print("Both lists recieved contain no values")
         yield
+    try:
 
-    elif len(lmList0[0]) >= 8:
-        try:
+        if len(lmList1) != 0 and len(lmList0) != 0: # Case 1: Both hands are visible
+            x1, y1, x2, y2 = lmList0[0][8][0], lmList0[0][8][1], lmList0[0][8][0], lmList0[0][8][1]
+            yield [x1, y1, x2, y2]
 
+        # Case 2: Only One hand is visible
+
+        if len(lmList0) > 0:
             x, y = lmList0[0][8][0], lmList0[0][8][1]
             yield [x, y]
 
-        except IndexError:
-            print("Index error here")
-
-    elif  len(lmList1[0]) >= 8:
-        try:
-            x, y = lmList0[0][8][0], lmList0[0][8][1]
+        elif  len(lmList1) >= 0:
+            x, y = lmList1[0][8][0], lmList1[0][8][1]
             yield [x, y]
 
-        except IndexError:
-            print("Index error here")
+        else:
+            print("Unknown error")
     
-    else:
-        print("Unknown error")
+    except IndexError:
+        print("Index error here")
+        print("Current List:\n")
+        print(lmList0, lmList1)
